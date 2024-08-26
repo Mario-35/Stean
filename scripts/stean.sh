17a4ebebcd2ed7c331c922eed535f3803af50c38
Adam <mario.adam@inrae.fr>
2024-08-26 09:16:58 +0200

    final scripts

--- scripts/stean.sh
+++ scripts/stean.sh
@@ -23,23 +23,6 @@ SQLSCRIPT=./script.sql
 STEANVER="not installed"
 ACTIVE=000
 
-function parse_json()
-{
-    echo $1 | \
-    sed -e 's/[{}]/''/g' | \
-    sed -e 's/", "/'\",\"'/g' | \
-    sed -e 's/" ,"/'\",\"'/g' | \
-    sed -e 's/" , "/'\",\"'/g' | \
-    sed -e 's/","/'\"---SEPERATOR---\"'/g' | \
-    awk -F=':' -v RS='---SEPERATOR---' "\$1~/\"$2\"/ {print}" | \
-    sed -e "s/\"$2\"://" | \
-    tr -d "\n\t" | \
-    sed -e 's/\\"/"/g' | \
-    sed -e 's/\\\\/\\/g' | \
-    sed -e 's/^[ \t]*//g' | \
-    sed -e 's/^"//'  -e 's/"$//'
-}
-
 # Create run script
 create_run_script() {
     if [ -f $FILERUN ]; then
