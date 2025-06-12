5e96726a818c7f8e68589dc7ae7201be8c228b6e
Adam <mario.adam@inrae.fr>
2025-06-12 23:32:36 +0200

    Add test web connection before test remote version

--- src/server/helpers/readRemoteVersion.ts
+++ src/server/helpers/readRemoteVersion.ts
@@ -6,7 +6,7 @@
  *
  */
 
-import { httpsDownloadJSON } from ".";
+import { connectWeb, httpsDownloadJSON } from ".";
 import { EConstant } from "../enums";
 import { Iversion } from "../types";
 
@@ -14,12 +14,14 @@ import { Iversion } from "../types";
  * Reads the applications version from the git repository.
  */
 export async function readRemoteVersion(): Promise<Iversion | undefined> {
-    try {
-        const file = EConstant.repository.replace("github.com", "raw.githubusercontent.com") + `/refs/heads/${EConstant.branch}/builds/stean_latest.info`;
-        const version = await httpsDownloadJSON(file, "stean_latest.info");
-        return version ? { version: version["version" as keyof object], date: version["date" as keyof object] } : undefined;
-    } catch (error) {
-        console.log(error);
-        return;
-    }
+    return connectWeb().then(async () => {
+        try {
+            const file = EConstant.repository.replace("github.com", "raw.githubusercontent.com") + `/refs/heads/${EConstant.branch}/builds/stean_latest.info`;
+            const version = await httpsDownloadJSON(file, "stean_latest.info");
+            return version ? { version: version["version" as keyof object], date: version["date" as keyof object] } : undefined;
+        } catch (error) {
+            console.log(error);
+            return;
+        }
+    });
 }
