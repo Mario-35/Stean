90fb858ab164103a61017b610954f2eff28d8030
Adam <mario.adam@inrae.fr>
2025-06-13 16:56:47 +0200

    Add test web connection before test remote version

--- src/server/models/helpers/createUpdateValues.ts
+++ src/server/models/helpers/createUpdateValues.ts
@@ -15,6 +15,6 @@ import { Ientity } from "../../types";
 export function createUpdateValues(entity: Ientity, input: Record<string, any>): string {
     console.log(log.whereIam());
     return Object.keys(input)
-        .map((elem: string) => `${doubleQuotesString(elem)} = ${input[elem][0] === "{" ? `${entity.columns[elem].dataType === EDataType.result || EDataType.jsonb ? "" : `COALESCE(${doubleQuotesString(elem)}, '{}'::jsonb) ||`} ${simpleQuotesString(ESCAPE_SIMPLE_QUOTE(input[elem]))}::jsonb` : simpleQuotesString(ESCAPE_SIMPLE_QUOTE(input[elem]))}`)
+        .map((elem: string) => `${doubleQuotesString(elem)} = ${input[elem][0] === "{" ? `${entity.columns[elem].dataType === EDataType.result || EDataType.jsonb ? "" : `COALESCE(${doubleQuotesString(elem)}, '{}'::jsonb) ||`} ${simpleQuotesString(ESCAPE_SIMPLE_QUOTE(input[elem]))}` : simpleQuotesString(ESCAPE_SIMPLE_QUOTE(input[elem]))}`)
         .join();
 }
