7d9a49356698234704f7a4adf0b4ea16cda82625
ADAM Mario <adam.mario@icloud.com>
2025-11-23 09:59:46 +0100

    update README

--- src/server/enums/constant.ts
+++ src/server/enums/constant.ts
@@ -38,5 +38,20 @@ export const EConstant = Object.freeze({
         defaultSrc: ["'self'"],
         scriptSrc: ["'self'", "'unsafe-inline'", "cdnjs.cloudflare.com"],
         styleSrc: ["'self'", "'unsafe-inline'", "cdnjs.cloudflare.com", "fonts.googleapis.com"]
+    },
+    sessionConfig: {
+        key: 'koa.sess', /** (string) cookie key (default is koa.sess) */
+        /** (number || 'session') maxAge in ms (default is 1 days) */
+        /** 'session' will result in a cookie that expires when session/browser is closed */
+        /** Warning: If a session cookie is stolen, this cookie will never expire */
+        maxAge: 86400000,
+        autoCommit: true, /** (boolean) automatically commit headers (default true) */
+        overwrite: true, /** (boolean) can overwrite or not (default true) */
+        httpOnly: true, /** (boolean) httpOnly or not (default true) */
+        signed: true, /** (boolean) signed or not (default true) */
+        rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+        renew: false, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+        secure: true, /** (boolean) secure cookie*/
+        sameSite: null, /** (string) session cookie sameSite options (default null, do not provide this key if you are not restricting sameSite) */
     }
 });
